bda5af1156cf3d8ebdf6af4eef2d42318c6cd435
ekansh-debug <ekansh123123@gmail.com>
2026-07-17 17:52:14 +0530

    display changes with touch

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 let gameBordState = [];
 
+const gameChoise = (function () {
+    let choise = 0;
+
+    function getChoise () {
+        if((choise++) % 2 === 0) return 'O';
+        return 'X';
+    }
+    return getChoise;
+})();
+const gameGridEvent = (e) => {
+    const currentGrid = e.currentTarget;
+
+    if (currentGrid.textContent){
+        return;
+    }
+
+    const currentGridChoise = gameChoise(); 
+    currentGrid.textContent = currentGridChoise;
+
+}
+const resetEvent = () => {
+    allGameGrid.forEach((gameGrid) => {
+        gameGrid.textContent = '';
+    })
+}
+
+
+const allGameGrid = document.querySelectorAll(".gameGrid");
+const reset = document.querySelector("#reset");
+
+reset.addEventListener('click',resetEvent);
+allGameGrid.forEach((gameGrid) => {
+    gameGrid.addEventListener("click",gameGridEvent);
+});
 
